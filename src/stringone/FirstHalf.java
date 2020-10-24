5609a6785db06c1c1d6ab5429a865d5a6fa05e05
aprilhoward <apehow111@gmail.com>
2020-10-24 12:29:21 -0400

    completed firstHalf method

--- src/stringone/FirstHalf.java
+++ src/stringone/FirstHalf.java
@@ -8,4 +8,14 @@ package stringone;
 //        firstHalf("HelloThere") → "Hello"
 //        firstHalf("abcdef") → "abc"
 public class FirstHalf {
+
+    public static String firstHalf(String str) {
+        return str.substring(0, str.length()/2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(firstHalf("WooHoo"));
+        System.out.println(firstHalf("HelloThere"));
+        System.out.println(firstHalf("abcdef"));
+    }
 }
