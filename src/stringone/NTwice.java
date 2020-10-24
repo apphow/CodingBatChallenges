b4a8b811f3659916466a8584565a3abebd93e153
aprilhoward <apehow111@gmail.com>
2020-10-24 13:24:29 -0400

    added nTwice challenge

---
+++ src/stringone/NTwice.java
@@ -0,0 +1,24 @@
+package stringone;
+
+//      Given a string and an int n, return a string
+//      made of the first and last n chars from the
+//      string. The string length will be at least n.
+//
+//
+//        nTwice("Hello", 2) → "Helo"
+//        nTwice("Chocolate", 3) → "Choate"
+//        nTwice("Chocolate", 1) → "Ce"
+
+public class NTwice {
+
+    public static String nTwice(String str, int n) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        nTwice("Hello", 2);
+        nTwice("Chocolate", 3);
+        nTwice("Colcolate", 1);
+    }
+
+}
