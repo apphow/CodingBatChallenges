5080613809447eba656181ef8f5e4734c8c1507d
aprilhoward <apehow111@gmail.com>
2020-10-24 12:48:21 -0400

    added theEnd challenge

---
+++ src/stringone/TheEnd.java
@@ -0,0 +1,24 @@
+package stringone;
+
+//        Given a string, return a string length 1 from its front,
+//        unless front is false, in which case return a string
+//        length 1 from its back. The string will be non-empty.
+//
+//
+//        theEnd("Hello", true) → "H"
+//        theEnd("Hello", false) → "o"
+//        theEnd("oh", true) → "o"
+public class TheEnd {
+
+    public static String theEnd(String str, boolean front) {
+
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        theEnd("Hello", true);
+        theEnd("Hello", false);
+        theEnd("oh", true);
+    }
+}
