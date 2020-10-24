0fe0c570048ade398b48e86ab8c870a4f860bfc3
aprilhoward <apehow111@gmail.com>
2020-10-24 13:30:48 -0400

    completed twoChar challenge

--- src/stringone/TwoChar.java
+++ src/stringone/TwoChar.java
@@ -12,7 +12,13 @@ package stringone;
 public class TwoChar {
 
     public static String twoChar(String str, int index) {
-        return null;
+
+        if(str.length() <= index + 1 || index < 0) {
+            System.out.println(str.substring(0, 2));
+            return str.substring(0, 2);
+        }
+        System.out.println(str.substring(index, index + 2));
+        return str.substring(index, index + 2);
     }
 
     public static void main(String[] args) {
