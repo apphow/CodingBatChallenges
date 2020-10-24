6392cd8bdf658ecab0106541f6235abf0cd1cc26
aprilhoward <apehow111@gmail.com>
2020-10-24 13:28:26 -0400

    added twoChar challenge from codingbat

---
+++ src/stringone/TwoChar.java
@@ -0,0 +1,23 @@
+package stringone;
+
+//      Given a string and an index, return a string
+//      length 2 starting at the given index.
+//      If the index is too big or too small to
+//      define a string length 2, use the first 2 chars. The string length will be at least 2.
+//
+//
+//        twoChar("java", 0) → "ja"
+//        twoChar("java", 2) → "va"
+//        twoChar("java", 3) → "ja"
+public class TwoChar {
+
+    public static String twoChar(String str, int index) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        twoChar("java", 0);
+        twoChar("java", 2);
+        twoChar("java", 3);
+    }
+}
