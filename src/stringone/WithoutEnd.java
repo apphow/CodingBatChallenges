1bd1c926112be05f3b02f67880b1fb7cd93d8ec7
aprilhoward <apehow111@gmail.com>
2020-10-24 12:31:58 -0400

    completed withoutEnd challenge

--- src/stringone/WithoutEnd.java
+++ src/stringone/WithoutEnd.java
@@ -12,6 +12,12 @@ public class WithoutEnd {
 //    withoutEnd("coding") → "odin"
 
     public static String withoutEnd(String str) {
-        return null;
+        return str.substring(1, str.length() - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(withoutEnd("Hello"));
+        System.out.println(withoutEnd("java"));
+        System.out.println(withoutEnd("coding"));
     }
 }
