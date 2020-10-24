16de03f122b9e0a959a636249c6839fd7c0c2c3a
aprilhoward <apehow111@gmail.com>
2020-10-24 12:30:35 -0400

    added withoutEnd challenge

---
+++ src/stringone/WithoutEnd.java
@@ -0,0 +1,17 @@
+package stringone;
+
+public class WithoutEnd {
+
+//    Given a string, return a version without the
+//    first and last char, so "Hello" yields "ell".
+//    The string length will be at least 2.
+//
+//
+//    withoutEnd("Hello") → "ell"
+//    withoutEnd("java") → "av"
+//    withoutEnd("coding") → "odin"
+
+    public static String withoutEnd(String str) {
+        return null;
+    }
+}
