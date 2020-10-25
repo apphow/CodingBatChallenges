70854ca0f79f9a33304ecf8e8aa56c54cd17faf4
aprilhoward <apehow111@gmail.com>
2020-10-25 13:16:51 -0400

    added withoutX2 challenge

---
+++ src/stringone/WithoutX2.java
@@ -0,0 +1,45 @@
+package stringone;
+
+public class WithoutX2 {
+
+//    Given a string, if the first or last chars are 'x',
+//    return the string without those 'x' chars, and
+//    otherwise return the string unchanged.
+//
+//    withoutX("xHix") → "Hi"
+//    withoutX("xHi") → "Hi"
+//    withoutX("Hxix") → "Hxi"
+//    withoutX2("xHi") → "Hi"
+//    withoutX2("Hxi") → "Hi"
+//    withoutX2("Hi") → "Hi"
+//    withoutX2("xxHi") → "Hi"
+//    withoutX2("Hix") → "Hix"
+//    withoutX2("xaxb") → "axb"
+//    withoutX2("xx") → ""	""
+//    withoutX2("x") → ""	""
+//    withoutX2("") → ""	""
+//    withoutX2("Hello") → "Hello"
+//    withoutX2("Hexllo") → "Hexllo"
+//    withoutX2("xHxllo") → "Hxllo"
+
+    public static String withoutX2(String str) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        withoutX2("xHix");
+        withoutX2("xHi");
+        withoutX2("Hxix");
+        withoutX2("Hxi");
+        withoutX2("Hi");
+        withoutX2("xxHi");
+        withoutX2("xaxb");
+        withoutX2("XX");
+        withoutX2("x");
+        withoutX2("");
+        withoutX2("Hello");
+        withoutX2("Hexllo");
+        withoutX2("xHxllo");
+
+    }
+}
