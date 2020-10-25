ee22db05aea24eeaf95a8bfaf32a7e0565797cae
aprilhoward <apehow111@gmail.com>
2020-10-25 13:56:33 -0400

    firstLast6 challenge complete

--- src/arrayone/firstLast6.java
+++ src/arrayone/firstLast6.java
@@ -10,11 +10,49 @@ package arrayone;
 //        firstLast6([13, 6, 1, 2, 3]) → false
 public class firstLast6 {
 
-    public boolean firstLast6(int[] nums) {
-        return true;
+    public static boolean firstLast6(int[] nums) {
+
+        int first = nums[0];
+        int last = nums[nums.length - 1];
+        if (first == 6 || last == 6) {
+            System.out.println(true);
+            return true;
+        }
+        System.out.println(false);
+        return false;
     }
 
     public static void main(String[] args) {
+        int[] one = {1, 2, 6};
+        int[] two = {6, 1, 2, 3};
+        int[] three = {13, 6, 1, 2, 3};
+        int[] four = {12, 6, 1, 2, 6};
+        int[] five = {3, 2, 1};
+        int[] six = {3, 6, 1};
+        int[] seven = {3, 6};
+        int[] eight = {6};
+        int[] nine = {3};
+        int[] ten = {5, 6};
+        int[] eleven = {5, 5};
+        int[] twelve = {1, 2, 3, 4, 6};
+        int[] uhoh = {1, 2, 3, 4};
+        firstLast6(one);
+        firstLast6(two);
+        firstLast6(three);
+        firstLast6(four);
+        firstLast6(five);
+        firstLast6(six);
+        firstLast6(seven);
+        firstLast6(eight);
+        firstLast6(nine);
+        firstLast6(ten);
+        firstLast6(eleven);
+        firstLast6(twelve);
+        firstLast6(uhoh);
+
+
+
+
 
     }
 }
