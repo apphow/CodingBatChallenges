10f08eb434f467f6e8028b0850b750e9f90c8c1a
aprilhoward <apehow111@gmail.com>
2020-10-27 14:09:08 -0400

    completed arrayCount9 challenge

---
+++ src/warmup2/arrayCount9.java
@@ -0,0 +1,31 @@
+package warmup2;
+// Given an array of ints, return the number of 9's in
+// the array.
+//
+// arrayCount9([1, 2, 9]) → 1
+// arrayCount9([1, 9, 9]) → 2
+// arrayCount9([1, 9, 9, 3, 9]) → 3
+
+public class arrayCount9 {
+    public static int arrayCount9(int[] nums) {
+        int count = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 9) {
+                count++;
+            }
+        }
+        System.out.println(count);
+        return count;
+    }
+
+    public static void main(String[] args) {
+        arrayCount9(new int[] {1, 2, 9});
+        arrayCount9(new int[] {1, 9, 9});
+        arrayCount9(new int[] {1, 9, 9, 3, 9});
+        arrayCount9(new int[] {1, 2, 3});
+        arrayCount9(new int[] {});
+        arrayCount9(new int[] {4, 2, 4, 3, 1});
+        arrayCount9(new int[] {9, 2, 4, 3, 1});
+
+    }
+}
