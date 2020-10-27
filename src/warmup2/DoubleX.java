0943d779d2961fac0b81fecc9e9b0b184c8198cf
aprilhoward <apehow111@gmail.com>
2020-10-27 13:37:02 -0400

    added doubleX challenge instructions

--- src/warmup2/DoubleX.java
+++ src/warmup2/DoubleX.java
@@ -1,5 +1,13 @@
 package warmup2;
 
+// Given a string, return true if the first instance
+// of "x" in the string is immediately followed by
+// another "x".
+//
+// doubleX("axxbb") → true
+// doubleX("axaxax") → false
+// doubleX("xxxxx") → true
+
 public class DoubleX {
     public static boolean doubleX(String str) {
         int i = str.indexOf("x");
