3f8c604c83dcf42d20bf87399ecd5831e82629f6
aprilhoward <apehow111@gmail.com>
2020-10-28 15:36:16 -0400

    added array667 coding bat challenge

---
+++ src/warmup2/Array667.java
@@ -0,0 +1,36 @@
+package warmup2;
+
+// Given an array of ints, return the number of
+// times that two 6's are next to each other in
+// the array. Also count instances where the
+// second "6" is actually a 7.
+//
+// array667([6, 6, 2]) → 1
+// array667([6, 6, 2, 6]) → 1
+// array667([6, 7, 2, 6]) → 1
+public class Array667 {
+
+    public static int array667(int[] nums) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        array667(new int[] {6, 6, 2});
+        array667(new int[] {6, 6, 2, 6});
+        array667(new int[] {6, 7, 2, 6});
+        array667(new int[] {6, 6, 2, 6, 7});
+        array667(new int[] {1, 6, 3});
+        array667(new int[] {6, 1});
+        array667(new int[] {});
+        array667(new int[] {3, 6, 7, 6});
+        array667(new int[] {3, 6, 6, 7});
+        array667(new int[] {6, 3, 6, 6});
+        array667(new int[] {6, 7, 6, 6});
+        array667(new int[] {1, 2, 3, 5, 6});
+        array667(new int[] {1, 2, 3, 6, 6});
+
+    }
+}
+
+
