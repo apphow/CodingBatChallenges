c9678f3742d58555e6f9214e7a05fc77abd74182
aprilhoward <apehow111@gmail.com>
2020-10-29 15:32:22 -0400

    solved array667 challenge

--- src/warmup2/Array667.java
+++ src/warmup2/Array667.java
@@ -11,7 +11,14 @@ package warmup2;
 public class Array667 {
 
     public static int array667(int[] nums) {
-        return -1;
+        int count = 0;
+        for(int i = 0; i < nums.length -1; i++) {
+            if((nums[i] == 6) && (nums[i + 1] == 6 || nums[i + 1] == 7)) {
+                count++;
+            }
+        }
+        System.out.println(count);
+        return count;
     }
 
     public static void main(String[] args) {
