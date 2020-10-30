ac569cb9856436d2666da179392d4b81a11ace4e
aprilhoward <apehow111@gmail.com>
2020-10-29 20:08:32 -0400

    completed old35 challenge

--- src/logicone/More20.java
+++ src/logicone/More20.java
@@ -5,7 +5,6 @@ import java.sql.SQLOutput;
 
 // Return true if the given non-negative number
 // is 1 or 2 more than a multiple of 20. See also:
-// Introduction to Mod
 //
 //        more20(20) → false
 //        more20(21) → true
